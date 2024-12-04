50b93b67514e25e0a1888829be4d35ac111d0e84
VoTranQuocToan <votranquoctoan2004@gmail.com>
2024-12-04 19:44:06 +0700

    TH1

---
+++ ASP.NET/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // GET: User
+        public ActionResult Resgister()
+        {
+            return View();
+        }
+    }
+}
